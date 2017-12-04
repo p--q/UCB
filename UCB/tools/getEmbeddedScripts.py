d672a45c58d71a1fdcc65d57ccd2f48f3a32946b
pq <pq.blogspot@gmail.com>
2017-12-05 00:16:11 +0900

    HM

---
+++ UCB/tools/getEmbeddedScripts.py
@@ -0,0 +1,99 @@
+#!/opt/libreoffice5.4/program/python
+# -*- coding: utf-8 -*-
+import unohelper  # オートメーションには必須(必須なのはuno)。
+import glob
+import os, sys
+from com.sun.star.embed import ElementModes  # 定数
+def macro():  
+	ctx = XSCRIPTCONTEXT.getComponentContext()  # コンポーネントコンテクストの取得。
+	smgr = ctx.getServiceManager()  # サービスマネージャーの取得。	
+	simplefileaccess = smgr.createInstanceWithContext("com.sun.star.ucb.SimpleFileAccess", ctx)  	
+	os.chdir("..")  # 一つ上のディレクトリに移動。
+	ods = glob.glob("*.ods")[0]  # odsファイルを取得。最初の一つのみ取得。
+	systempath = os.path.join(os.getcwd(), ods)  # odsファイルのフルパス。
+	doc_fileurl = unohelper.systemPathToFileUrl(systempath)
+	storagefactory = smgr.createInstanceWithContext('com.sun.star.embed.StorageFactory', ctx)
+	documentstorage = storagefactory.createInstanceWithArguments((doc_fileurl, ElementModes.READ))
+	scriptsstorage = documentstorage.openStorageElement("Scripts", ElementModes.READ)
+	pythonstorage = scriptsstorage.openStorageElement("python", ElementModes.READ)
+	src_path = os.path.join(os.getcwd(), "src")
+	if not os.path.exists(src_path):
+		os.mkdir(src_path) 
+	os.chdir(src_path)
+	
+# 	if not simplefileaccess.exists(embeddedmacropath):  # 埋め込みマクロフォルダが存在しないとき。
+# 		simplefileaccess.createFolder(embeddedmacropath)  # 埋め込みマクロフォルダを作成する。urlの最後に/がついていても、途中のフォルダがなくても作成してくれる。ただし、中身を入れないとmanifest.xmlに記録されるだけ。
+
+	
+	for name in pythonstorage:
+		if pythonstorage.isStorageElement(name):
+			os.mkdir(name)
+			
+			
+			
+		
+		elif pythonstorage.isStreamElement(name):
+			stream = pythonstorage.cloneStreamElement(name)
+			simplefileaccess.writeFile(''.join((dir_url, name)), stream.getInputStream())
+
+
+	
+	
+	
+	
+	
+
+
+
+g_exportedScripts = macro, #マクロセレクターに限定表示させる関数をタプルで指定。		
+if __name__ == "__main__":  # オートメーションで実行するとき
+	def automation():  # オートメーションのためにglobalに出すのはこの関数のみにする。
+		import officehelper
+		from functools import wraps
+		import sys
+		from com.sun.star.beans import PropertyValue  # Struct
+		from com.sun.star.script.provider import XScriptContext  
+		def connectOffice(func):  # funcの前後でOffice接続の処理
+			@wraps(func)
+			def wrapper():  # LibreOfficeをバックグラウンドで起動してコンポーネントテクストとサービスマネジャーを取得する。
+				try:
+					ctx = officehelper.bootstrap()  # コンポーネントコンテクストの取得。
+				except:
+					print("Could not establish a connection with a running office.", file=sys.stderr)
+					sys.exit()
+				print("Connected to a running office ...")
+				smgr = ctx.getServiceManager()  # サービスマネジャーの取得。
+				print("Using {} {}".format(*_getLOVersion(ctx, smgr)))  # LibreOfficeのバージョンを出力。
+				return func(ctx, smgr)  # 引数の関数の実行。
+			def _getLOVersion(ctx, smgr):  # LibreOfficeの名前とバージョンを返す。
+				cp = smgr.createInstanceWithContext('com.sun.star.configuration.ConfigurationProvider', ctx)
+				node = PropertyValue(Name = 'nodepath', Value = 'org.openoffice.Setup/Product' )  # share/registry/main.xcd内のノードパス。
+				ca = cp.createInstanceWithArguments('com.sun.star.configuration.ConfigurationAccess', (node,))
+				return ca.getPropertyValues(('ooName', 'ooSetupVersion'))  # LibreOfficeの名前とバージョンをタプルで返す。
+			return wrapper
+		@connectOffice  # createXSCRIPTCONTEXTの引数にctxとsmgrを渡すデコレータ。
+		def createXSCRIPTCONTEXT(ctx, smgr):  # XSCRIPTCONTEXTを生成。
+			class ScriptContext(unohelper.Base, XScriptContext):
+				def __init__(self, ctx):
+					self.ctx = ctx
+				def getComponentContext(self):
+					return self.ctx
+				def getDesktop(self):
+					return ctx.getByName('/singletons/com.sun.star.frame.theDesktop')  # com.sun.star.frame.Desktopはdeprecatedになっている。
+				def getDocument(self):
+					return self.getDesktop().getCurrentComponent()
+			return ScriptContext(ctx)  
+		XSCRIPTCONTEXT = createXSCRIPTCONTEXT()  # XSCRIPTCONTEXTの取得。
+# 		doc = XSCRIPTCONTEXT.getDocument()  # 現在開いているドキュメントを取得。
+# 		doctype = "scalc", "com.sun.star.sheet.SpreadsheetDocument"  # Calcドキュメントを開くとき。
+	# 	doctype = "swriter", "com.sun.star.text.TextDocument"  # Writerドキュメントを開くとき。
+# 		if (doc is None) or (not doc.supportsService(doctype[1])):  # ドキュメントが取得できなかった時またはCalcドキュメントではない時
+# 			XSCRIPTCONTEXT.getDesktop().loadComponentFromURL("private:factory/{}".format(doctype[0]), "_blank", 0, ())  # ドキュメントを開く。ここでdocに代入してもドキュメントが開く前にmacro()が呼ばれてしまう。
+# 		flg = True
+# 		while flg:
+# 			doc = XSCRIPTCONTEXT.getDocument()  # 現在開いているドキュメントを取得。
+# 			if doc is not None:
+# 				flg = (not doc.supportsService(doctype[1]))  # ドキュメントタイプが確認できたらwhileを抜ける。
+		return XSCRIPTCONTEXT
+	XSCRIPTCONTEXT = automation()  # XSCRIPTCONTEXTを取得。	
+	macro()  # マクロの実行。
