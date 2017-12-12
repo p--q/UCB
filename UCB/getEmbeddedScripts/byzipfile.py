65b37d4bee7dc7a2853b0c68a45c3624d14c9e09
pq <pq.blogspot@gmail.com>
2017-12-12 23:35:53 +0900

    HM

--- UCB/getEmbeddedScripts/byzipfile.py
+++ UCB/getEmbeddedScripts/byzipfile.py
@@ -4,17 +4,17 @@ import glob
 import os
 from zipfile import ZipFile
 import shutil
-def macro():  
+def macro():  # オートメーションでのみ実行可。 
 	os.chdir("..")  # 一つ上のディレクトリに移動。
 	ods = glob.glob("*.ods")[0]  # odsファイルを取得。最初の一つのみ取得。
-	odszip = ZipFile(ods , 'r')
 	src_path = os.path.join(os.getcwd(), "src")  # srcフォルダのパスを取得。
-	embeddemacro_path = "Scripts/python/"
-	output_path = "/".join((src_path, embeddemacro_path))
-	if os.path.exists(output_path):
-		shutil.rmtree(output_path)
-	[odszip.extract(name, path=src_path) for name in odszip.namelist() if name.startswith(embeddemacro_path)]
-	print("Extract the embedded macro folder from {}.".format(ods))
+	embeddemacro_path = "Scripts/python/"  # ドキュメント内のパス。
+	output_path = "/".join((src_path, embeddemacro_path))  # 出力先フォルダのパス。
+	if os.path.exists(output_path):  # 出力先フォルダが存在する時。
+		shutil.rmtree(output_path)  # 出力先フォルダを削除。
+	with ZipFile(ods , 'r') as odszip: # odsファイルをZipFileで読み取り専用で開く。
+		[odszip.extract(name, path=src_path) for name in odszip.namelist() if name.startswith(embeddemacro_path)]  # Scripts/python/から始まるパスのファイルのみ出力先フォルダに解凍する。
+		print("Extract the embedded macro folder from {}.".format(ods))
 if __name__ == "__main__":  # オートメーションで実行するとき	
 	macro()
 	
